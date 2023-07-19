d58eabc4382c47566a7dc05330da9afc7b6dac7e
OpenInfoEnergy <open.infoenergy@gmail.com>
2023-07-19 18:21:14 +0200

    zwei kleine Python Programme, die völlig unnütz sind"

---
+++ lokaleDatei.py
@@ -0,0 +1,2 @@
+print ("Dies ist eine lokale Datei")
+print ("Sie kommt nicht über das Webinterface rein, sondern wird hier direkt auf dem lokalen Rechner erzeugt")
---
+++ nocheineDatei.py
@@ -0,0 +1,6 @@
+import time
+
+time.sleep(1.0)
+"Wartet genau eine Sekunde"
+
+
