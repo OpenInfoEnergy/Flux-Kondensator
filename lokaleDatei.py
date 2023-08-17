7204587e64a2fb7826ad716255a493336f2ec7ee
OpenInfoEnergy <open.infoenergy@gmail.com>
2023-08-17 18:13:44 +0200

    wieder zurueck an F.

--- lokaleDatei.py
+++ lokaleDatei.py
@@ -2,3 +2,4 @@ print ("Dies ist eine lokale Datei")
 print ("Sie kommt nicht über das Webinterface rein, sondern wird hier direkt auf dem lokalen Rechner erzeugt")
 hier ein kommentar.
 Und hier ist auch wieder was.
+Und dann schreibe ich hier was an Franziska
